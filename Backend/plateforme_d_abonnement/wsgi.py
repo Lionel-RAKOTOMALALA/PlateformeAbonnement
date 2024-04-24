5d4647000220f167867d0545b6876e5c8f2ab241
Lionel-RAKOTOMALALA <rakotomalalalionel32@gmail.com>
2024-04-24 03:03:37 +0300

    first commit

---
+++ Backend/plateforme_d_abonnement/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for plateforme_d_abonnement project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plateforme_d_abonnement.settings')
+
+application = get_wsgi_application()
